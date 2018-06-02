f3d8fd586e19e1f0c8abd5acde680fa7c2a60ba4
David King <davidcking.inbox@gmail.com>
2018-06-01 20:17:10 -0600

    cmd line message for bin/leaves.js

--- bin/leaves.js
+++ bin/leaves.js
@@ -2,6 +2,11 @@
 
 var treetools = require('../index');
 
+if (process.argv.length < 3) {
+    console.log(process.argv[1] + " tree.newick");
+    process.exit(1);
+}
+
 var filename = process.argv[2];
 var nwf = treetools.parseFile(filename, main, error);
 
